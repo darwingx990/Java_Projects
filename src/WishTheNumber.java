8bc2f1d70bdb6663cc3a47dd3b210f88a639e599
Darwing <Darwingx990@gmail.com>
2024-09-24 19:26:11 -0500

    Game created: 'Wish the number from 1 to 50.'

--- src/WishTheNumber.java
+++ src/WishTheNumber.java
@@ -1,15 +1,17 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class WishTheNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int randomNumber = (int) (Math.random() * 100);
+        int wishedNumber = 50;
+        int randomNumber = new Random().nextInt(100);
         System.out.println("""
                 ===============================
                 |       WISH THE NUMBER       |
                 ===============================""");
         for (int i = 0; i != randomNumber; i++) {
-            System.out.println("Enter a number between 1 and 100");
+            System.out.println("Enter a number from 1 to %d".formatted(wishedNumber));
             int number = sc.nextInt();
 
             if (number < randomNumber) {
